b6e8bb230b155b2817471f922e34b60e1ab68e7e
audrlo <loaudr@gmail.com>
2025-08-06 12:43:04 -0700

    Create test_version_2.py

---
+++ test_version_2.py
@@ -0,0 +1,40 @@
+from roboclaw import Roboclaw
+import time
+
+roboclaw = Roboclaw("/dev/ttyACM0", 115200)
+roboclaw.Open()
+
+address = 0x80
+
+result, version = roboclaw.ReadVersion(address)
+print("Success:", result)
+print("Version:", version)
+
+roboclaw.ForwardM1(address, 64)  # Half speed
+print("Move???")
+time.sleep(2)
+roboclaw.ForwardM1(address, 0)   # Stop
+print("stopped")
+
+# Check voltage
+status, volts = roboclaw.ReadMainBatteryVoltage(address)
+print("Battery voltage:", volts / 10.0, "V")
+
+# Try using signed duty mode
+print("Trying DutyM1...")
+roboclaw.DutyM1(address, 32767)
+roboclaw.DutyM2(address, -32767)
+time.sleep(2)
+roboclaw.DutyM1(address, 0)
+roboclaw.DutyM2(address, 0)
+print("Done.")
+
+time.sleep(5)
+
+print("Trying Speed")
+roboclaw.SpeedM1(address, 10000)  # 10k encoder ticks/sec
+roboclaw.SpeedM2(address, -10000)
+time.sleep(5)
+roboclaw.SpeedM1(address, 0)
+roboclaw.SpeedM2(address, 0)
+print("Done")
